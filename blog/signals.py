f1f7e7fdaf79161bb17e489343a8099170bc72a5
Mahyar Amiri <mmaahhyyaarr@gmail.com>
2023-04-13 14:36:52 +0330

    update signals.py

--- blog/signals.py
+++ blog/signals.py
@@ -4,9 +4,10 @@ from decouple import config
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from comment.models import Comment
-from rating.models import Rating
-from reaction.models import Reaction
+from comment.models import Comment, Reaction
+from rating.models import UserRating
+from reaction.models import UserReaction
+
 
 chat_id = config('CHAT_ID')
 TOKEN = config('TOKEN')
@@ -14,15 +15,28 @@ bot = telebot.TeleBot(TOKEN)
 
 
 @receiver(post_save, sender=Comment, dispatch_uid="comment")
-def comment_created(sender, instance, **kwargs):
-    bot.send_message(chat_id, instance)
+def comment(sender, instance, **kwargs):
+    comment_type = f'replied to #{instance.parent.user.username}' if instance.parent else 'commented'
+    comment_content = instance.content_main if instance.status_edited != 'd' else f'<s>{instance.content_main}</s>\n{instance.content}'
+    comment_content = f'<tg-spoiler>{comment_content}</tg-spoiler>' if instance.is_spoiler else comment_content
+    message = f'#Comment\n\n#{instance.user.username} {comment_type} on <b>{instance.content_object}</b>\n\n<i>{comment_content}</i>'
+    bot.send_message(chat_id, message, parse_mode='HTML')
 
 
-@receiver(post_save, sender=Rating, dispatch_uid="rating")
-def comment_created(sender, instance, **kwargs):
-    bot.send_message(chat_id, instance)
+@receiver(post_save, sender=Reaction, dispatch_uid="comment_reaction")
+def comment_reaction(sender, instance, **kwargs):
+    message = f'#Comment_Reaction\n\n#{instance.user.username} reacted {instance.react.emoji} to <b>{instance.comment.content_object}</b> comment.\n\n#{instance.comment.user.username} : \n{instance.comment.content_main}'
+    bot.send_message(chat_id, message, parse_mode='HTML')
 
 
-@receiver(post_save, sender=Reaction, dispatch_uid="reaction")
-def comment_created(sender, instance, **kwargs):
-    bot.send_message(chat_id, instance)
+@receiver(post_save, sender=UserRating, dispatch_uid="rating")
+def rating(sender, instance, **kwargs):
+    user_rate = f'[{instance.rate}]⭐' if instance.rating.settings.slug == 'default-config' else '❤️'
+    message = f'#Rating\n\n#{instance.user.username} rated {user_rate} to <b>{instance.rating.content_object}</b>'
+    bot.send_message(chat_id, message, parse_mode='HTML')
+
+
+@receiver(post_save, sender=UserReaction, dispatch_uid="reaction")
+def reaction(sender, instance, **kwargs):
+    message = f'#Reaction\n\n#{instance.user.username} reacted {instance.react.emoji} to <b>{instance.reaction.content_object}</b>'
+    bot.send_message(chat_id, message, parse_mode='HTML')
