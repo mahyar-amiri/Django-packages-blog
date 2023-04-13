5e5f03ee2aa3c1282b1aeb8536485f3299220dcf
Mahyar Amiri <mmaahhyyaarr@gmail.com>
2023-04-13 09:07:27 +0330

    add create comment post_save

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,23 @@
-from comment.models import Comment
+import telebot
+from decouple import config
+
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from comment.models import Comment
 from rating.models import Rating
 from reaction.models import Reaction
+
+
+@receiver(post_save, sender=Comment, dispatch_uid="comment_created")
+def comment_created(sender, instance, **kwargs):
+    chat_id = config('CHAT_ID')
+    TOKEN = config('TOKEN')
+    bot = telebot.TeleBot(TOKEN)
+    bot.send_message(chat_id, instance)
+    # print(instance)
 
 
 class Article(models.Model):
